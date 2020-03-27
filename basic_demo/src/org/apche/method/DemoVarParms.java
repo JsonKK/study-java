5e152287cd270855b227a61d1b354efc1536a160
lsujie <595675516@qq.com>
2020-03-27 15:33:26 +0800

    增加可变参数

---
+++ basic_demo/src/org/apche/method/DemoVarParms.java
@@ -0,0 +1,23 @@
+package org.apche.method;
+
+public class DemoVarParms {
+    //可变参数求和
+    public void sum(int[]arr,int...n){
+        int sum = 0;
+        for (int i:arr){
+            sum += i;
+        }
+        for(int i:n){
+            sum += i;
+        }
+        System.out.println("总数为："+sum);
+    }
+
+    public static void main(String[] args) {
+        DemoVarParms demoVarParms = new DemoVarParms();
+        int[] arr = {1,2,};
+        demoVarParms.sum(arr,3);
+        demoVarParms.sum(arr,3,4);
+        demoVarParms.sum(arr,3,4,5);
+    }
+}
